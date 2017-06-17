4f34c2c1c96b36755caff80c3855ece95362cc6a
jsirianni <jsirianni@ws1.sir.local>
2017-06-17 00:25:09 -0400

    dev

--- http_server/scripts/status.php
+++ http_server/scripts/status.php
@@ -9,7 +9,7 @@ echo "\n";
 //system("ip addr show | grep 'global eth0'");
 
 // Get service information
-//system("systemctl status mysql | grep Active");
+echo exec('systemctl status mysql | grep Active');
 //system("systemctl status apache2 | grep Active");
 
 // Get disk information
