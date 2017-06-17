cdbc80029d8536f438b14d122b95dbc9b8dc1aa4
jsirianni <jsirianni@ws1.sir.local>
2017-06-16 23:20:43 -0400

    dev

--- http_server/scripts/checksql.php
+++ http_server/scripts/checksql.php
@@ -1,46 +1,21 @@
 <?php
 
-//
-// Set global values
-//
-function setGlobal() {
-  date_default_timezone_set("America/Detroit");
-  $GLOBALS['dbhost'] = 'localhost';
-  $GLOBALS['dbname'] = 'report';
-  $GLOBALS['ddbuser'] = 'reportuser';
-  $GLOBALS['dbuserpass'] = 'password';
+include 'query.php';
+
+// Connect to db
+$sqlconn = dbConnect();
+
+// Get sql
+$sql = $GET_['sql'];
+
+// Execute query
+$result = mysqli_query($sqlconn,$sql);
+$sqlconn->close();
+
+// Return true if result is not false
+if ($result != 'FALSE') {
+  echo "true";
 }
-
-//
-// Return true or false
-//
-function check($sql) {
-  setGlobal();
-  $sqlconn =  mysqli_connect($GLOBALS['dbhost'], $GLOBALS['ddbuser'], $GLOBALS['dbuserpass'], $GLOBALS['dbname']);
-  if (!mysqli_query($sqlconn,$sql)) {
-    $sqlconn->close();
-    echo "false";
-    return;
-  }
-  else {
-    $sqlconn->close();
-    echo "true";
-    return;
-  }
+else {
+  echo "false";
 }
-
-// Begin script
-//if($_SERVER['REQUEST_METHOD']=="GET") {
-//  $function = $_GET['call'];
-//  if(function_exists($function)) {
-//    call_user_func($function;
-//  }
-//  else {
-//    echo 'Function Not Exists!!';
-//  }
-//}
-
-$query = $_GET['sql'];
-check("SELECT * FROM report.reports");
-
- ?>
