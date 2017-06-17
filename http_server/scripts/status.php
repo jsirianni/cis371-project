64ebd2396580ba7ab1bc334c342dfb7033951bc3
jsirianni <jsirianni@ws1.sir.local>
2017-06-17 00:14:03 -0400

    dev

--- http_server/scripts/status.php
+++ http_server/scripts/status.php
@@ -2,27 +2,22 @@
 include 'query.php'
 
 // Get system uptime
-$uptime = exec("uptime");
+echo exec("uptime");
 
 // Get IP information
-$ifconfig = exec("ip addr show | grep 'global eth0'");
+echo "<br>";
+echo exec("ip addr show | grep 'global eth0'");
 
 // Get service information
-$mysqlStatus = exec("systemctl status mysql | grep Active");
-$apacheStatus = exec("systemctl status apache2 | grep Active");
+echo "<br>";
+echo exec("systemctl status mysql | grep Active");
+echo "<br>";
+echo exec("systemctl status apache2 | grep Active");
 
 // Get disk information
-$disk = exec("df -h | grep /dev/sda1");
+echo "<br>";
+echo exec("df -h | grep /dev/sda1");
 
-echo "$uptime";
-echo "<br>";
-echo "$ifconfig";
-echo "<br>";
-echo "$mysqlStatus";
-echo "<br>";
-echo "$apacheStatus";
-echo "<br>";
-echo "$disk";
 
 
 ?>
