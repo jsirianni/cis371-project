006073b5c6e66945fad928867d4948782175184c
jsirianni <jsirianni@ws1.sir.local>
2017-06-17 00:28:19 -0400

    dev

--- http_server/scripts/status.php
+++ http_server/scripts/status.php
@@ -2,6 +2,7 @@
 
 
 // Get system uptime
+echo "<p>System Uptime</p>";
 echo exec('uptime');
 echo "\n<br>";
 
@@ -9,12 +10,16 @@ echo "\n<br>";
 //system("ip addr show | grep 'global eth0'");
 
 // Get service information
+echo "<p>MySQL Status</p>";
 echo exec('systemctl status mysql | grep Active');
 echo "\n<br>";
+
+echo "<p>Apache Status</p>";
 echo exec('systemctl status apache2 | grep Active');
+echo "\n<br>";
 
 // Get disk information
-echo "\n<br>";
+echo "<p>Disk Usage</p>";
 echo exec('df -h | grep /dev/sda1');
 
 
