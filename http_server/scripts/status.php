44014342a8222ee0a2282ed348a8588cd1803e77
jsirianni <jsirianni@ws1.sir.local>
2017-06-17 20:40:49 -0400

    misc

--- http_server/scripts/status.php
+++ http_server/scripts/status.php
@@ -26,7 +26,7 @@ echo "\n<br>";
 
 // Get disk information
 echo "<h4>Disk Usage</h4>";
-echo exec('df -h | grep /dev/sda1');
+echo "Process: " . exec('df -h | grep /dev/sda1');
 echo "\n<br>";
 
 echo "</div>";
